f7d47c2595e8927082cb422e60a69e695256c09b
Masaki Matsushita <glass.saga@gmail.com>
2014-12-16 19:50:28 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             'minimal'
+maintainer       'NTT Communications'
+maintainer_email 'masaki.matsushita@ntt.com'
+license          'All rights reserved'
+description      'Installs/Configures minimal_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.13'
+depends          'users'
+depends          'sudo'
+depends          'yum-epel'
+depends          'git'
+depends          'openssl'
+depends          'build-essential'
---
+++ recipes/default.rb
@@ -0,0 +1,68 @@
+include_recipe "users::sysadmins"
+
+sudo "sysadmin" do
+  group "sysadmin"
+  nopasswd true
+end
+
+file "/etc/ssh/sshd_config" do
+  _file = Chef::Util::FileEdit.new(path)
+  _file.search_file_replace("#PasswordAuthentication yes", "PasswordAuthentication no")
+  _file.search_file_replace("PermitRootLogin yes", "PermitRootLogin no")
+  _file.search_file_replace("PermitRootLogin without-password", "PermitRootLogin no")
+  content _file.send(:editor).lines.join
+end
+
+if platform_family?("debian")
+  if platform?("ubuntu")
+    sudo "ubuntu" do
+      user "ubuntu"
+      nopasswd true
+    end
+
+    file "/etc/apt/sources.list" do
+      _file = Chef::Util::FileEdit.new(path)
+      _file.search_file_replace("us.archive.ubuntu.com", "ftp.jaist.ac.jp")
+      _file.search_file_replace("jp.archive.ubuntu.com", "ftp.jaist.ac.jp")
+      _file.search_file_replace("archive.ubuntu.com", "ftp.jaist.ac.jp")
+      _file.search_file_replace("security.ubuntu.com", "ftp.jaist.ac.jp")
+      content _file.send(:editor).lines.join
+      notifies :run, 'execute[apt-get update]', :immediately
+    end
+
+    execute "apt-get update" do
+      action :nothing
+    end
+  end
+
+  apt_package "language-pack-ja"
+end
+
+link "/etc/localtime" do
+  to "/usr/share/zoneinfo/Japan"
+end
+
+package "zsh"
+include_recipe "yum-epel" if platform_family?("rhel")
+include_recipe "git"
+include_recipe "build-essential"
+include_recipe "openssl"
+
+package "traceroute"
+package "tcptraceroute" if platform?("mac_os_x")
+package "nc" if platform_family?("rhel")
+package "vim"
+package "htop"
+package "sysv-rc-conf" if platform?("ubuntu")
+
+=begin
+# update
+case node[:platform_family]
+when "rhel"
+  execute "yum update -y"
+when "debian"
+  execute "apt-get update && apt-get dist-upgrade -y"
+when "mac_os_x"
+  execute "brew update"
+end
+=end
