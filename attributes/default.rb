eb97b74dbc978d8a44375865a9a127734f2ee3a7
Masaki Matsushita <glass.saga@gmail.com>
2015-07-27 18:09:01 +0900

    remove ntp servers

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,15 +8,7 @@ default[:tz] = "Asia/Tokyo"
 
 # ntp
 default[:ntp][:servers] = [
-  "ntp.nict.jp",
-  "ntp1.jst.mfeed.ad.jp",
-  "ntp2.jst.mfeed.ad.jp",
-  "ntp3.jst.mfeed.ad.jp",
-  "ats1.e-timing.ne.jp",
-  "x.ns.gin.ntt.net",
-  "y.ns.gin.ntt.net",
-  "ntp-tk01.ocn.ad.jp",
-  "ntp-tk02.ocn.ad.jp"
+  "ntp.nict.jp"
 ]
 
 # hostname
